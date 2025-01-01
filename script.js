464a2efe2f0c88b19d138c819eb3b946a3804e54
ScaredBow <31490393+ScaredBow@users.noreply.github.com>
2024-12-31 20:32:31 -0800

    fixed bg music

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Get the audio element
-const music = document.getElementById('background-music');
+const music = document.getElementById('iframeAudio');
 
 // Set the volume (0 is silent, 1 is full volume)
 music.volume = 0.4; // 50% volume for subtle background music
