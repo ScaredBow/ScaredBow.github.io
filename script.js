23e7ebcc2e7a4a9f8a689056369b25f6c1e66910
ScaredBow <31490393+ScaredBow@users.noreply.github.com>
2024-12-31 20:09:28 -0800

    bug fixes. music readded and scaled to 40%

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get the audio element
+const music = document.getElementById('background-music');
+
+// Set the volume (0 is silent, 1 is full volume)
+music.volume = 0.4; // 50% volume for subtle background music
