da7bd55a5c36cd3c65ed15e8574ddacd716e5556
ScaredBow <31490393+ScaredBow@users.noreply.github.com>
2024-12-31 19:53:52 -0800

    Bug fixes

--- script.js
+++ script.js
@@ -2,4 +2,4 @@
 const music = document.getElementById('background-music');
 
 // Set the volume (0 is silent, 1 is full volume)
-music.volume = 0.2; // 20% volume
+music.volume = 0.2; // 20% volume for subtle background music
