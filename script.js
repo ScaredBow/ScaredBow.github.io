5539c9d941fefd4e3a85bcad91226aedcb95f520
ScaredBow <31490393+ScaredBow@users.noreply.github.com>
2024-12-31 19:58:40 -0800

    revert

--- script.js
+++
@@ -1,5 +0,0 @@
-// Get the audio element
-const music = document.getElementById('background-music');
-
-// Set the volume (0 is silent, 1 is full volume)
-music.volume = 0.2; // 20% volume for subtle background music
